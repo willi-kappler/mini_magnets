b0cf6c15a91e750803c63c87bb5f2f1c00a56793
Willi Kappler <willi.kappler.gm@gmail.com>
2019-06-06 07:59:47 +0200

    Try SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,55 @@
-// #[macro_use] extern crate quick_error;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
 
-extern crate piston_window;
+// Needed libraries:
+// apt install libsdl2-2.0-0 libsdl2-dev libsdl2-gfx-1.0-0 libsdl2-gfx-dev libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-mixer-2.0-0 libsdl2-mixer-dev libsdl2-ttf-2.0-0 libsdl2-ttf-dev
 
-use piston_window::*;
+// Game loop:
+// https://gameprogrammingpatterns.com/game-loop.html
+// https://gamedev.stackexchange.com/questions/69753/game-loop-best-way-to-limit-the-fps
+// http://www.java-gaming.org/topics/game-loops/24220/view.html
+// https://bell0bytes.eu/the-game-loop/
+// https://www.reddit.com/r/pcmasterrace/comments/29qcqr/an_explanation_of_game_loops_fps_and_delta_time/
+// http://www.informit.com/articles/article.aspx?p=2167437&seqNum=3
 
-fn main() {
-    println!("Hello World");
 
-    let (screen_width, screen_height) = (640.0, 480.0);
-
-    let mut window: PistonWindow = WindowSettings::new("Hello Piston!",
-        (screen_width as u32, screen_height as u32))
-        .exit_on_esc(false)
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+ 
+    let window = video_subsystem.window("Mini-Magnets", 800, 600)
+        .position_centered()
         .build()
-        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+        .unwrap();
+ 
+    let mut canvas = window.into_canvas().build().unwrap();
+ 
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
 
-    let mut x = 0.0;
-    let mut y = 0.0;
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
 
-    let mut vx = 0.0;
-    let mut vy = 0.0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
 
-    let mut accelx = 0.0;
-    let mut accely = 0.0;
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
+            }
+        }
+        // The rest of the game loop goes here...
 
-    let accel = 0.01;
-
-    let friction = 0.99;
-
-    let (sizex, sizey) = (50.0, 50.0);
-
-    let borderx = screen_width - sizex;
-    let bordery = screen_height - sizey;
-
-    while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g| {
-            clear([0.0, 0.0, 0.0, 1.0], g); // black background
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [x, y, sizex, sizey], // rectangle
-                      c.transform, g);
-        });
-
-        x = x + vx;
-        y = y + vy;
-
-        if x < 0.0 { vx = -vx; x = 0.0; }
-        else if x > borderx { vx = -vx; x = borderx; }
-        if y < 0.0 { vy = -vy; y = 0.0; }
-        else if y > bordery { vy = -vy; y = bordery; }
-
-        vx = vx + accelx;
-        vy = vy + accely;
-
-        vx = vx * friction;
-        vy = vy * friction;
-
-        match e.press_args() {
-            Some(Button::Keyboard(Key::Up)) => accely = accely - accel,
-            Some(Button::Keyboard(Key::Down)) => accely = accely + accel,
-            Some(Button::Keyboard(Key::Left)) => accelx = accelx - accel,
-            Some(Button::Keyboard(Key::Right)) => accelx = accelx + accel,
-            _ => ()
-        };
-
-        match e.release_args() {
-            Some(Button::Keyboard(Key::Up)) | Some(Button::Keyboard(Key::Down)) => accely = 0.0,
-            Some(Button::Keyboard(Key::Left)) | Some(Button::Keyboard(Key::Right)) => accelx = 0.0,
-            _ => ()
-        };
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
