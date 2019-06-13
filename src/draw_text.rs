188d6a3208060a618530fb0aa06a66ede96c558a
Willi Kappler <willi.kappler.gm@gmail.com>
2019-06-13 23:20:51 +0200

    Remove sdl-ttf, more chars from ASCII table

--- src/draw_text.rs
+++ src/draw_text.rs
@@ -17,28 +17,19 @@ pub struct Font<'a> {
     pub texture: Texture<'a>,
 }
 
-// let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/characters.bmp"))?;
-// https://docs.rs/sdl2/0.32.2/sdl2/surface/struct.Surface.html#method.load_bmp
-
-// let texture = texture_creator.create_texture_from_surface(&temp_surface).map_err(|e| e.to_string())?;
-// https://docs.rs/sdl2/0.32.2/sdl2/render/struct.TextureCreator.html#method.create_texture_from_surface
-
-// canvas.copy_ex(&texture, Some(source_rect_0), Some(dest_rect_0), 0.0, None, false, false)?;
-// canvas.copy(&texture, Some(source_rect_0), Some(dest_rect_0))?;
-// https://docs.rs/sdl2/0.32.2/sdl2/render/struct.Canvas.html#method.copy
-
-
-
 // ASCII table:
 //
 // https://de.wikipedia.org/wiki/American_Standard_Code_for_Information_Interchange
 //
-// 32   Space
-// 48 0 Zero
-// 49 1 One
-// 57 9 Nine
-// 65 A Capital A
-// 90 Z Capital Z
+// 32   Space       33 !        34 "        35 #
+// 36 $             37 %        38 &        39 '
+// 40 (             41 )        42 *        43 +
+// 44 ,             45 -        46 .        47 /
+// 48 0 Zero        49 1        57 9        58 :
+// 59 ;             60 <        61 =        62 >
+// 63 ?             64 @        65 A        90 Z
+// 91 [             92 \        93 ]        94 ^
+// 95 _
 
 pub fn draw_char(canvas: &mut Canvas<Window>, font: &Font, x: u32, y: u32, c: u8) {
     let index: u8;
